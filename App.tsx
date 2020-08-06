fc62f64b67820b63656474f5a3295b24cb180552
Ronald Glover <ron881212@gmail.com>
2020-08-06 11:57:56 -0400

    Added navigation pkgs

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your pets!!!!</Text>
+        </View>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 

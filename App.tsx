91b8ee4f331ae0bc42d446633df09f85ddb5c172
Ronald Glover <ron881212@gmail.com>
2021-08-11 06:37:24 -0400

    Added necessary set up for login in and sign up screens

--- App.tsx
+++ App.tsx
@@ -1,23 +1,55 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
-import DrawerTab from './navigation/drawTab'
-import BottomTab from './navigation/bottomTab'
+import React, {useEffect, useState} from 'react';
+// import {StyleSheet, Text, View} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import DrawerTab from './navigation/drawTab';
+import {Provider as ReduxProvider} from 'react-redux';
+import {StatusBar} from 'expo-status-bar';
+// import {createStore, combineReducers} from 'redux';
+import WalkthroughScreen from './navigation/WalkthroughScreen';
+
+// const rootReducer = combineReducers({
+//   setUser,
+//   otherReducers
+// All of our reducers go here seperated by commas
+// });
+
+// const store = createStore(rootReducer); // store for our ReduxProvider
 
 export default function App() {
+  const [user, setUser] = useState(null);
+
+  // useEffect(() => {
+  //   const unsubscriber = firebase.auth().onAuthStateChanged((currentUser) => {
+  //     setUser(currentUser);
+  //   });
+
+  //   return () => {
+  //     unsubscriber();
+  //   };
+  // }, [user]);
+
   return (
-    <PaperProvider>
-      <DrawerTab />
-    </PaperProvider>
+    // <ReduxProvider store={store}>
+    //   {user ? (
+    //     <PaperProvider>
+    //       <DrawerTab />
+    //     </PaperProvider>
+    //   ) : (
+    <>
+      <StatusBar />
+      <WalkthroughScreen />
+    </>
+    //   )}
+    // </ReduxProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ navigation/SignUp.tsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class SignUp extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    );
+  }
+}
+
+// const styles = StyleSheet.create({})

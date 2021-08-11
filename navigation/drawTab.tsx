fbfd8ccc19b40eb4dfce0687ea76bd4657158122
Ronald Glover <ron881212@gmail.com>
2021-08-11 06:38:00 -0400

    no change linting

--- navigation/drawTab.tsx
+++ navigation/drawTab.tsx
@@ -1,35 +1,28 @@
-import React from 'react'
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  useWindowDimensions
-} from 'react-native';
-import { NavigationContainer, StackActions } from '@react-navigation/native'
-import { createDrawerNavigator } from '@react-navigation/drawer'
-import BottomTab from './bottomTab'
-import Draw from '../screens/Draw'
+import React from 'react';
+import {useWindowDimensions} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import BottomTab from './bottomTab';
+import Draw from '../screens/Draw';
 
-const RootDrawer = createDrawerNavigator()
+const RootDrawer = createDrawerNavigator();
 
-export default function drawTab(){
-    const dimensions = useWindowDimensions();
-    const isLargeScreen = dimensions.width >= 768;
+export default function DrawTab() {
+  const dimensions = useWindowDimensions();
+  const isLargeScreen = dimensions.width >= 768;
 
-    return (
-      <NavigationContainer>
-        <RootDrawer.Navigator
-          drawerType={isLargeScreen ? 'permanent' : 'front'}
-          drawerStyle={isLargeScreen ? null : { width: '50%' }}
-          drawerPosition="right"
-          overlayColor="transparent"
-          drawerContent={(props)=> { return <Draw {...props}/> } }
-        >
-          <RootDrawer.Screen name="Bottom" component={BottomTab} />
-        </RootDrawer.Navigator>
-      </NavigationContainer>
-    );
-  };
+  return (
+    <NavigationContainer>
+      <RootDrawer.Navigator
+        drawerType={isLargeScreen ? 'permanent' : 'front'}
+        drawerStyle={isLargeScreen ? null : {width: '50%'}}
+        drawerPosition="right"
+        overlayColor="transparent"
+        drawerContent={(props) => {
+          return <Draw {...props} />;
+        }}>
+        <RootDrawer.Screen name="Bottom" component={BottomTab} />
+      </RootDrawer.Navigator>
+    </NavigationContainer>
+  );
+}
--- navigation/nearby.tsx
+++ navigation/nearby.tsx
@@ -1,41 +1,43 @@
 import * as React from 'react';
-import { View, StyleSheet, TouchableOpacity, Image } from 'react-native'
-import { Button, Card, Surface, Text, Paragraph, List } from 'react-native-paper';
-import Menu from '../assets/homes/menu.svg'
-import Back from '../assets/homes/back.svg'
+import {View, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import {Button, Card, Surface, Text, Paragraph, List} from 'react-native-paper';
+import Menu from '../assets/homes/menu.svg';
+import Back from '../assets/homes/back.svg';
 
-export default function Nearby({navigation}:any) {
-    return(
-        <View style={styles.container}>
-        <List.Item
-          title='Nearby'
-          titleStyle={styles.title}
-          right={props => 
-            <TouchableOpacity onPress={()=>navigation.openDrawer()} style={styles.seeAll}>
+export default function Nearby({navigation}: any) {
+  return (
+    <View style={styles.container}>
+      <List.Item
+        title="Nearby"
+        titleStyle={styles.title}
+        right={(props) => (
+          <TouchableOpacity
+            onPress={() => navigation.openDrawer()}
+            style={styles.seeAll}>
             <Text>See all</Text>
-            </TouchableOpacity>
-          }
-          style={{padding:0}}
-        />
-        </View>
-    )
+          </TouchableOpacity>
+        )}
+        style={{padding: 0}}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flexGrow: 1,
+  container: {
+    flexGrow: 1,
     //   alignItems: 'center',
     //   justifyContent: 'center',
-    },
-    title: {
-        color:'black', 
-        fontSize:20, 
-        fontWeight: "500",
-        textAlign:'left',
-        marginLeft:20
-    },
-    seeAll: {
-        alignSelf:'flex-end', 
-        marginRight:20
-    }
-})
+  },
+  title: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: '500',
+    textAlign: 'left',
+    marginLeft: 20,
+  },
+  seeAll: {
+    alignSelf: 'flex-end',
+    marginRight: 20,
+  },
+});
